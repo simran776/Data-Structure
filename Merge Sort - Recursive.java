e738218bd18be0f64f177bf4cdf2077268cb7dc9
Gia Huang <huangjiebupt@gmail.com>
2015-05-28 19:53:33 -0400

    Create Merge Sort - Recursive.java

---
+++ Merge Sort - Recursive.java
@@ -0,0 +1,91 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{	
+	/* l is for left index and r is right index of the sub-array
+  		of arr to be sorted */
+	public static void mergeSort(int arr[], int l, int r){
+		if (l < r){
+      		int m = l+(r-l)/2; //Same as (l+r)/2 but avoids overflow for large l & h
+      		mergeSort(arr, l, m);
+      		mergeSort(arr, m+1, r);
+      		merge(arr, l, m, r);
+   		}
+	}
+	
+	/* Function to merge the two haves arr[l..m] and arr[m+1..r] of array arr[] */
+	public static void merge(int arr[], int l, int m, int r){
+    	int i, j, k;
+    	int n1 = m - l + 1;
+    	int n2 =  r - m;
+ 
+    	/* create temp arrays */
+    	int[] L = new int[n1];
+    	int[] R = new int[n2];
+ 
+    	/* Copy data to temp arrays L[] and R[] */
+    	for (i = 0; i < n1; i++)
+        	L[i] = arr[l + i];
+    	for (j = 0; j < n2; j++)
+        	R[j] = arr[m + 1+ j];
+ 
+    	/* Merge the temp arrays back into arr[l..r]*/
+    	i = 0;
+    	j = 0;
+    	k = l;
+    	while (i < n1 && j < n2){
+        	if (L[i] <= R[j]){
+            	arr[k] = L[i];
+            	i++;
+        	}
+        	else{
+            	arr[k] = R[j];
+            	j++;
+        	}
+        	k++;
+    	}
+ 
+    	/* Copy the remaining elements of L[], if there are any */
+    	while (i < n1){
+        	arr[k] = L[i];
+        	i++;
+        	k++;
+    	}
+ 
+    	/* Copy the remaining elements of R[], if there are any */
+    	while (j < n2){
+        	arr[k] = R[j];
+        	j++;
+        	k++;
+    	}
+	}
+	
+	public static void printArray(int A[], int size){
+    	int i;
+    	for (i=0; i < size; i++)
+        	System.out.print(A[i]+ " ");
+    	System.out.println();
+	}
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		System.out.println("Hello!");
+		int[] arr = {12, 11, 13, 5, 6, 7};
+    	int arr_size = arr.length;
+ 
+    	System.out.println("Given array is \n");
+    	printArray(arr, arr_size);
+ 
+    	mergeSort(arr, 0, arr_size - 1);
+ 
+    	System.out.println("\nSorted array is \n");
+    	printArray(arr, arr_size);
+    	return;
+	}
+}
