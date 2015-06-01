0230334911560031a5fd763505a28a2d80d8e31e
Gia Huang <huangjiebupt@gmail.com>
2015-06-01 19:24:31 -0400

    Create Count 1’s in a sorted binary array.java

---
+++ Search-and-Sorting/Count 1’s in a sorted binary array.java
@@ -0,0 +1,13 @@
+/**
+* Given a binary array sorted in non-increasing order, count the number of 1’s in it. 
+
+Examples: 
+Input: arr[] = {1, 1, 0, 0, 0, 0, 0}
+Output: 2
+
+Input: arr[] = {1, 1, 1, 1, 1, 1, 1}
+Output: 7
+
+Input: arr[] = {0, 0, 0, 0, 0, 0, 0}
+Output: 0
+**/
