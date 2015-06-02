1c46ff99dcfabc6c5fcbaeb8376f0ac4c5b942d9
Gia Huang <huangjiebupt@gmail.com>
2015-06-01 20:38:05 -0400

    Update Find the minimum cost to reach destination using a train.java

--- Greedy-DP/Find the minimum cost to reach destination using a train.java
+++ Greedy-DP/Find the minimum cost to reach destination using a train.java
@@ -30,14 +30,65 @@ minCost(0, N-1) = MIN { cost[0][n-1],
                         minCost(0, N-2) + cost[N-2][n-1] } 
                         
 Time complexity of the above implementation is exponential as it tries every possible path from 0 to N-1.
-
-Solution 2: DP
-One dynamic programming solution is to create a 2D table and fill the table using above given recursive formula. The extra space required in this solution would be O(N2) and time complexity would be O(N3)
-
-We can solve this problem using O(N) extra space and O(N2) time. The idea is based on the fact that given input matrix is a Directed Acyclic Graph (DAG). 
 **/
 
 /* package whatever; // don't place package name! */
+
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{	
+	// A recursive function to find the shortest path from
+	// source 's' to destination 'd'.
+	public static int minCostRec(int[][] cost, int s, int d)
+	{
+    	// If source is same as destination or destination is next to source
+    	if(s == d || s+1 == d)
+      		return cost[s][d];
+ 
+    	// Initialize min cost as direct ticket from source 's' to destination 'd'.
+    	int min = cost[s][d];
+ 
+    	// Try every intermediate vertex to find minimum
+    	for(int i = s+1; i<d; i++)
+    	{
+        	int c = minCostRec(cost, s, i) + minCostRec(cost, i, d);
+        	if (c < min)
+           		min = c;
+    	}
+    	return min;
+	}
+ 
+	// This function returns the smallest possible cost to
+	// reach station N-1 from station 0. This function mainly uses minCostRec().
+	public static int minCost(int[][] cost)
+	{
+    	return minCostRec(cost, 0, cost.length-1);
+	}
+
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int[][] cost = { {0, 15, 80, 90},
+                      {Integer.MAX_VALUE, 0, 40, 50},
+                      {Integer.MAX_VALUE, Integer.MAX_VALUE, 0, 70},
+                      {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 0}
+                    };
+    	System.out.println("The Minimum cost to reach station " + cost.length + " is " + minCost(cost));
+	}
+}
+
+/**
+ * Solution 2: DP
+One dynamic programming solution is to create a 2D table and fill the table using above given recursive formula. The extra space required in this solution would be O(N2) and time complexity would be O(N3)
+We can solve this problem using O(N) extra space and O(N2) time. The idea is based on the fact that given input matrix is a Directed Acyclic Graph (DAG). 
+ * */
+
 
 import java.util.*;
 import java.lang.*;
@@ -56,10 +107,10 @@ class Ideone
  
     	// Go through every station and check if using it
     	// as an intermediate station gives better path
-    	for(int i=0; i<N; i++){
-    		for(int j=i+1; j<N; j++){
-          		if (dist[j] > dist[i] + cost[i][j])
-             		dist[j] = dist[i] + cost[i][j];
+    	for(int i=1; i<N; i++){
+    		for(int j=0; j<i; j++){
+    			if(dist[i]>dist[j]+cost[j][i])
+    				dist[i] = dist[j]+cost[j][i];
     		}
     	}
     	return dist[N-1];
