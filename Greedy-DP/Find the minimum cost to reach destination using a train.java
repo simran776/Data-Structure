b57cacf0284209279ac2b681415feff1c43db7b5
Gia Huang <huangjiebupt@gmail.com>
2015-06-01 20:11:38 -0400

    Update Find the minimum cost to reach destination using a train.java

--- Greedy-DP/Find the minimum cost to reach destination using a train.java
+++ Greedy-DP/Find the minimum cost to reach destination using a train.java
@@ -36,3 +36,42 @@ One dynamic programming solution is to create a 2D table and fill the table usin
 
 We can solve this problem using O(N) extra space and O(N2) time. The idea is based on the fact that given input matrix is a Directed Acyclic Graph (DAG). 
 **/
+
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{	public static int minCost(int[][] cost)
+	{	int N = cost.length;
+    	// dist[i] stores minimum cost to reach station i from station 0.
+    	int[] dist = new int[N];
+    	for(int i=0; i<N; i++)
+       		dist[i] = Integer.MAX_VALUE;
+    	
+    	dist[0] = 0;
+ 
+    	// Go through every station and check if using it
+    	// as an intermediate station gives better path
+    	for(int i=0; i<N; i++){
+    		for(int j=i+1; j<N; j++){
+          		if (dist[j] > dist[i] + cost[i][j])
+             		dist[j] = dist[i] + cost[i][j];
+    		}
+    	}
+    	return dist[N-1];
+	}
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		  int[][] cost = { {0, 15, 80, 90},
+                      {Integer.MAX_VALUE, 0, 40, 50},
+                      {Integer.MAX_VALUE, Integer.MAX_VALUE, 0, 70},
+                      {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 0}
+                    };
+    	System.out.println("The Minimum cost to reach station " + cost.length + " is " + minCost(cost));
+	}
+}
